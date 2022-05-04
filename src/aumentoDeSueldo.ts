148a59412fe2b5b5510e96f0c0395603de59212d
Melina Figueroa <104174303+Melina-figueroa@users.noreply.github.com>
2022-05-04 17:30:45 -0300

    aumento quite la variable total y modifique el porcentaje (10%)

--- src/aumentoDeSueldo.ts
+++ src/aumentoDeSueldo.ts
@@ -1,22 +1,18 @@
 let sueldoActual: number = Number(prompt("Sueldo actual")); //valor ingresado por teclado
 let aumento: number = 0;
-let total: number = sueldoActual + aumento;
 
 if (sueldoActual >= 0 && sueldoActual <= 15000) {
   //sueldo actual 0-15.000(20%)
   aumento = 1.2 * sueldoActual;
-  total = sueldoActual + aumento;
-  console.log("Sueldo con aumento:", total, "(Su aumento es del 20%)");
+  console.log("Sueldo con aumento:", aumento, "(Su aumento es del 20%)");
 } else if (sueldoActual >= 15001 && sueldoActual <= 20000) {
   //sueldo actual 15.001- 20.000(10%)
-  aumento = 1.0 * sueldoActual;
-  total = sueldoActual + aumento;
-  console.log("Sueldo con aumento:", total, "(Su aumento es del 10%)");
+  aumento = 1.1 * sueldoActual;
+  console.log("Sueldo con aumento:", aumento, "(Su aumento es del 10%)");
 } else if (sueldoActual >= 20001 && sueldoActual <= 25000) {
   //sueldo actual 20.001-25.000(5%)
   aumento = 1.05 * sueldoActual;
-  total = sueldoActual + aumento;
-  console.log("Sueldo con aumento:", total, "(Su aumento es del 5%)");
+  console.log("Sueldo con aumento:", aumento, "(Su aumento es del 5%)");
 } else {
   aumento = sueldoActual; //mas de 25.000 (no hay aumento)
   console.log("No corresponde aumento");
